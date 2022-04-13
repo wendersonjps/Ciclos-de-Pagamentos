64b8ddf7363ca26ffda873e138b93e36822e38a5
Wenderson Júnio <wendersonjps@gmail.com>
2022-04-13 14:21:11 -0300

    :sparkles: Added Sidebar component

---
+++ frontend/src/common/template/Sidebar.jsx
@@ -0,0 +1,12 @@
+/* import React from 'react' */
+import Menu from './Menu'
+
+export default function Sidebar(props) {
+    return (
+        <aside className="main-sidebar">
+            <section className="sidebar">
+                <Menu />
+            </section>
+        </aside>
+    )
+}
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -2,11 +2,13 @@ import '../common/template/dependencies'
 /* import React from 'react' */
 
 import Header from '../common/template/Header'
+import Sidebar from '../common/template/Sidebar'
 
 export default function App(props) {
     return (
         <div className="wrapper">
             <Header />
+            <Sidebar />
         </div>
     )
 }
