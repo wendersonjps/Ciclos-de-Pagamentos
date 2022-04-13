fd04c5b79a01f19bba240693a6107ec1a4d89f7e
Wenderson Júnio <wendersonjps@gmail.com>
2022-04-13 14:54:48 -0300

    :sparkles: Added Footer component

---
+++ frontend/src/common/template/Footer.jsx
@@ -0,0 +1,12 @@
+/* import React from 'react' */
+
+export default function Footer(props) {
+    return (
+        <footer className="main-footer">
+            <strong>
+                Copyright © 2022 WendersonJPS
+                <a href="https://github.com/wendersonjps" target='_blank'>GitHub</a>.
+            </strong>
+        </footer>
+    )
+}
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -3,12 +3,17 @@ import '../common/template/dependencies'
 
 import Header from '../common/template/Header'
 import Sidebar from '../common/template/Sidebar'
+import Footer from '../common/template/Footer'
 
 export default function App(props) {
     return (
         <div className="wrapper">
             <Header />
             <Sidebar />
+            <div className="content-wrapper">
+                <h1>Content</h1>
+            </div>
+            <Footer />
         </div>
     )
 }
