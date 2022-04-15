aecab8e95d76aefad3697e35392a08bb19dd6255
Wenderson Júnio <wendersonjps@gmail.com>
2022-04-15 11:41:29 -0300

    :hammer: Added Tabs

--- frontend/src/billingCycle/BillingCycle.jsx
+++ frontend/src/billingCycle/BillingCycle.jsx
@@ -2,6 +2,9 @@
 
 import ContentHeader from '../common/template/ContentHeader'
 import Content from '../common/template/Content'
+import Tab from '../common/tab/Tabs'
+import TabsHeader from '../common/tab/TabsHeader'
+import TabContent from '../common/tab/TabContent'
 
 class BillingCycle extends Component {
     render() {
@@ -9,7 +12,14 @@ class BillingCycle extends Component {
             <div>
                 <ContentHeader title='Ciclos de Pagamentos' subtitle='Cadastro' />
                 <Content>
+                    <Tabs>
+                        <TabsHeader>
 
+                        </TabsHeader>
+                        <TabContent>
+                            
+                        </TabContent>
+                    </Tabs>
                 </Content>
             </div>
         )
