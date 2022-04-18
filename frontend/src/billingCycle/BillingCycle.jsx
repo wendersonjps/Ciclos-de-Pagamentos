29cc04d7f0ab8eefd039ce58e6ec25efe10e5252
Wenderson Júnio <wendersonjps@gmail.com>
2022-04-18 12:32:41 -0300

    :hammer: Integrating BillingCycle with Redux

--- frontend/src/billingCycle/BillingCycle.jsx
+++ frontend/src/billingCycle/BillingCycle.jsx
@@ -1,4 +1,6 @@
-/* import React, { Component } from 'react' */
+/* import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux' */
 
 import ContentHeader from '../common/template/ContentHeader'
 import Content from '../common/template/Content'
@@ -7,9 +9,15 @@ import TabsHeader from '../common/tab/TabsHeader'
 import TabsContent from '../common/tab/TabContent'
 import TabHeader from '../common/tab/TabHeader'
 import TabContent from '../common/tab/TabContent'
+import { selectTab } from '../common/tab/tabActions'
 
 
 class BillingCycle extends Component {
+
+    componentWillMount() {
+        this.props.selectTab('tabList')
+    }
+
     render() {
         return (
             <div>
@@ -35,4 +43,6 @@ class BillingCycle extends Component {
     }
 }
 
-export default BillingCycle
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab }, dispatch)
+
+export default connect(null, mapDispatchToProps)(BillingCycle)
