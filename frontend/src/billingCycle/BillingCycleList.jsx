162770db08c0971b445ff7e7ea35c4f7bc3a3e09
Wenderson Júnio <wendersonjps@gmail.com>
2022-04-19 14:10:58 -0300

    :hammer: Displaying the data in BillingCycleList

--- frontend/src/billingCycle/BillingCycleList.jsx
+++ frontend/src/billingCycle/BillingCycleList.jsx
@@ -11,6 +11,17 @@ class BillingCycleList extends Component {
         console.log(this.props.list)
     }
 
+    renderRows() {
+        const list = this.props.list || []
+        return list.map(bc => (
+            <tr key={bc._id}>
+                <td>{bc.name}</td>
+                <td>{bc.month}</td>
+                <td>{bc.year}</td>
+            </tr>
+        ))
+    }
+
     render() {
         return (
             <div>
@@ -23,7 +34,7 @@ class BillingCycleList extends Component {
                         </tr>
                     </thead>
                     <tbody>
-
+                        {this.renderRows()}
                     </tbody>
                 </table>
             </div>
