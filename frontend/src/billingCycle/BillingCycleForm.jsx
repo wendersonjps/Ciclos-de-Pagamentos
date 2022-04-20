3ca408e4d3bc3ba59622fffab042dac9c90e413a
Wenderson Júnio <wendersonjps@gmail.com>
2022-04-20 11:55:16 -0300

    :hammer: Importing reduxForm and Field from ReduxForm

--- frontend/src/billingCycle/BillingCycleForm.jsx
+++ frontend/src/billingCycle/BillingCycleForm.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import { reduxForm, Field } from 'redux-form'
 
 class BillingCycleForm extends Component {
     render() {
-        return (
-            <form role="form">
-                <div className="box-body">
+        const { handleSubmit } = this.props
 
+        return (
+            <form role="form" onSubmit={handleSubmit}>
+                <div className="box-body">
+                    <Field name="name" component="input" />
+                    <Field name="month" component="input" />
+                    <Field name="year" component="input" />
                 </div>
                 <div className="box-footer">
                     <button type="submit" className="btn btn-primary">Submit</button>
@@ -15,4 +20,4 @@ class BillingCycleForm extends Component {
     }
 }
 
-export default BillingCycleForm
+export default reduxForm({ form: 'billingCycleForm' })(BillingCycleForm)
