5c3f4994cbdf21479486030da19daf2c782773dc
Wenderson Júnio <wendersonjps@gmail.com>
2022-04-20 14:07:19 -0300

    :hammer: Added new function ShowUpdate

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -33,3 +33,10 @@ export function create(values) {
         }
     }
 }
+
+export function showUpdate(billingCycle) {
+    return [
+        showTabs('tabUpdate'),
+        selectTab('tabUpdate')
+    ]
+}
