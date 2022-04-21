ed266ce34390eeb47a96f4ef13428760280b7aa9
Wenderson Júnio <wendersonjps@gmail.com>
2022-04-21 16:36:16 -0300

    :coffin: Removed Routes component

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -4,7 +4,6 @@ import React from 'react'
 import Header from '../common/template/Header'
 import Sidebar from '../common/template/Sidebar'
 import Footer from '../common/template/Footer'
-import Routes from './routes'
 import Messages from '../common/msg/Messages'
 
 export default function App(props) {
@@ -13,7 +12,7 @@ export default function App(props) {
             <Header />
             <Sidebar />
             <div className="content-wrapper">
-                <Routes />
+                {props.children}
             </div>
             <Footer />
             <Messages />
