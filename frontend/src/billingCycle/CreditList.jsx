9cef980493f5e5b27088642f66e883cf96e5d16f
Wenderson Júnio <wendersonjps@gmail.com>
2022-04-21 13:09:39 -0300

    :hammer: Integrating .map into the Field component

--- frontend/src/billingCycle/CreditList.jsx
+++ frontend/src/billingCycle/CreditList.jsx
@@ -7,13 +7,14 @@ import Input from '../common/form/Input'
 class CreditList extends Component {
 
     renderRows() {
-        return (
-            <tr>
-                <td><Field name="credits[0].name" component={Input} placeholder="Informe o nome" readOnly={this.props.readOnly} /></td>
-                <td><Field name="credits[0].value" component={Input} placeholder="Informe o valor" readOnly={this.props.readOnly} /></td>
+        const list = this.props.list || []
+        return list.map((item, index) => (
+            <tr key={index}>
+                <td><Field name={`credits[${index}].name`} component={Input} placeholder="Informe o nome" readOnly={this.props.readOnly} /></td>
+                <td><Field name={`credits[${index}].value`} component={Input} placeholder="Informe o valor" readOnly={this.props.readOnly} /></td>
                 <td></td>
             </tr>
-        )
+        ))
     }
 
     render() {
