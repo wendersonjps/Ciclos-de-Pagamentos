100fae571e249644a78db597a8ba76985192ffab
Wenderson Júnio <wendersonjps@gmail.com>
2022-04-21 16:26:09 -0300

    :lipstick: Adjusting Menu responsiveness

--- frontend/src/common/template/MenuItem.jsx
+++ frontend/src/common/template/MenuItem.jsx
@@ -4,7 +4,7 @@ export default function MenuItem(props) {
     return (
         <li>
             <a href={props.path}>
-                <i className={`fa fa-${props.icon}`}></i> {props.label}
+                <i className={`fa fa-${props.icon}`}></i> <span>{props.label}</span>
             </a>
         </li>
     )
