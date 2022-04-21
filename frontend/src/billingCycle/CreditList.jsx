0ea7957b80d0d79111d3e5a0efed1a9b62a5c7f0
Wenderson Júnio <wendersonjps@gmail.com>
2022-04-21 04:06:44 -0300

    :hammer: Integrating Input function into the Field component

--- frontend/src/billingCycle/CreditList.jsx
+++ frontend/src/billingCycle/CreditList.jsx
@@ -2,14 +2,15 @@ import React, { Component } from 'react'
 import { Field } from 'redux-form'
 
 import Grid from '../common/layout/Grid'
+import Input from '../common/form/Input'
 
 class CreditList extends Component {
 
     renderRows() {
         return (
             <tr>
-                <td><Field name="credits[0].name" component="input" /></td>
-                <td><Field name="credits[0].value" component="input" /></td>
+                <td><Field name="credits[0].name" component={Input} placeholder="Informe o nome" readOnly={this.props.readOnly} /></td>
+                <td><Field name="credits[0].value" component={Input} placeholder="Informe o valor" readOnly={this.props.readOnly} /></td>
                 <td></td>
             </tr>
         )
