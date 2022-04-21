97f50b3b0917d5889ad11b0e90f951f081a5c56e
Wenderson Júnio <wendersonjps@gmail.com>
2022-04-21 04:07:43 -0300

    :hammer: Integrating readOnly into the CreditList tag

--- frontend/src/billingCycle/BillingCycleForm.jsx
+++ frontend/src/billingCycle/BillingCycleForm.jsx
@@ -17,7 +17,7 @@ class BillingCycleForm extends Component {
                     <Field name="name" component={LabelAndInput} readOnly={readOnly} label="Nome" cols="12 4" placeholder="Informe o nome" />
                     <Field name="month" component={LabelAndInput} readOnly={readOnly} type="number" label="Mês" cols="12 4" placeholder="Informe o mês" />
                     <Field name="year" component={LabelAndInput} readOnly={readOnly} type="number" label="Ano" cols="12 4" placeholder="Informe o ano" />
-                    <CreditList cols='12 6' />
+                    <CreditList cols='12 6' readOnly={readOnly} />
                 </div>
                 <div className="box-footer">
                     <button type="submit" className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
