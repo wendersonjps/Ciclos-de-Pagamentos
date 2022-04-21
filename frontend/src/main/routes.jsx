7cd3bc6c27342b5cf4b82bf768cd3721db40fae2
Wenderson Júnio <wendersonjps@gmail.com>
2022-04-21 16:35:38 -0300

    :hammer: Importing and integrating IndexRoute and App component into the Route

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { Router, Route, Redirect, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 
+import App from './App'
 import Dashboard from '../dashboard/Dashboard'
 import BillingCycle from '../billingCycle/BillingCycle'
 
 export default function Routes(props) {
     <Router history={hashHistory}>
-        <Route path='/' component={Dashboard} />
-        <Route path='/billingCycle' component={BillingCycle} />
-        <Redirect from='*' to='/' />
+        <Route path="/" component={App}>
+            <IndexRoute component={Dashboard} />
+            <Route path="billingCycle" component={BillingCycle} />
+        </Route>
+        <Redirect from="*" to="/" />
     </Router>
 }
