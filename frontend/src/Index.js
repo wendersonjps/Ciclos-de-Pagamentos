d8359fcb6b9d3377deace81dab32da20f9022628
Wenderson Júnio <wendersonjps@gmail.com>
2022-04-26 12:47:27 -0300

    :truck: File renamed from .jsx to .js

--- frontend/src/Index.js
+++ frontend/src/Index.js
@@ -9,6 +9,7 @@ import promise from 'redux-promise'
 
 import Routes from './main/routes'
 import reducers from './main/reducers'
+import registerServiceWorker from './registerServiceWorker'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
@@ -16,4 +17,5 @@ ReactDOM.render(
     <Provider store={store}>
         <Routes />
     </Provider>
-    , document.getElementById('app'))
+, document.getElementById('app'))
+registerServiceWorker()
