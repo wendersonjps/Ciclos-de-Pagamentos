5cc247d146eb582fdcc173f4cd3ebf15bf731dbc
Wenderson Júnio <wendersonjps@gmail.com>
2022-04-26 13:15:54 -0300

    :wrench: Update lib

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
+import { Switch, Route, Redirect } from 'react-router'
 
 import AuthOrApp from './AuthOrApp'
 import Dashboard from '../dashboard/Dashboard'
 import BillingCycle from '../billingCycle/BillingCycle'
 
 export default function Routes(props) {
-    <Router history={hashHistory}>
-        <Route path="/" component={AuthOrApp}>
-            <IndexRoute component={Dashboard} />
-            <Route path="billingCycle" component={BillingCycle} />
-        </Route>
-        <Redirect from="*" to="/" />
-    </Router>
+    <div className="content-wrapper">
+        <Switch>
+            <Route exact path='/' component={Dashboard} />
+            <Route exact path='billingCycle' component={BillingCycle} />
+            <Route from="*" to="/" />
+        </Switch>
+    </div>
 }
